48a7894b40d391a7008e52f0e9b4be2ceb83bcf5
Ml.Leitenant <maxim.divnich@mail.ru>
2025-10-18 16:27:36 +0300

    Начало проекта

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.catsgram.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.Instant;
+
+@Data
+@EqualsAndHashCode(of = { "email" })
+public class User {
+    private long id;
+    private String username;
+    private String password;
+    private String email;
+    private Instant registrationDate;
+}
